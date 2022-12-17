937d555877616dd494187e9ff41c7a7b526b6b53
Ananya Seth <sethananya58@gmail.com>
2022-12-17 21:29:00 +0530

    Improvising code

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="CSS/styles.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+<?php include 'menu.php'; ?>
+
+<div class="jumbotron">
+    <a style="color: #367E18"><img src="images\logo_greenLeaf.png" alt="Logo" width="30" height=""></a>
+  <h1 style="color: #367E18">&nbsp;GREEN LEAF</h1>
+  <p style="color: #38E54D">&nbsp;&nbsp;&nbsp;GO GREEN | GROW GREEN | LIVE GREEN</p>
+</div>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="images\Green5.jpg" alt="Green5" class="img-fluid aboutimg">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+      <h2 class="display-4">We are GreenLeaf.</h2>
+      <p class="py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. </p>
+        <a href="about.php" target=_blank class="btn btn-success">More about us </a>
+    </div>
+  </div>
+</div>
+</section>
+
+<footer>
+  <p style="color: #367E18" class="p-3 bg-light text-center">@GreenLeafIndia</p>
+</footer>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a style="color: #367E18" class="navbar-brand" href="#">
+      <img src="images\logo_greenLeaf.png" alt="Logo" width="30" height="">
+    GreenLeaf</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a style="color: #38E54D" class="nav-link active" aria-current="page" href="index.php"><strong>Home</strong></a>
+        </li>
+        <li class="nav-item">
+          <a style="color: #38E54D" class="nav-link" href="#" target=_blank><strong>Shop</strong></a>
+        </li>
+        <li class="nav-item">
+          <a style="color: #38E54D" class="nav-link" href="about.php" target=_blank><strong>About</strong></a>
+        </li>
+        <li class="nav-item">
+          <a style="color: #38E54D" class="nav-link" href="#"><strong>Contact</strong></a>
+        </li>
+        <li class="nav-item">
+          <a style="color: #38E54D" class="nav-link" href="#"><strong>FAQ</strong></a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
