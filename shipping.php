75c6e6629b9ab2b26e8f809db37acb10455f9f55
Ananya Seth <sethananya58@gmail.com>
2022-12-19 19:11:09 +0530

    Shipping page

---
+++ shipping.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="CSS/styles_shipping.css">
+    </head>
+    <body>
+        <div class="container">
+            <form action="">
+                <div class="row">
+                    <div class="col">
+                        <h3 class="title">billing details</h3>
+                        <div class="inputbox">
+                            <span>Full Name :</span>
+                            <input type="text" placeholder="John Doe">
+                        </div>
+                        <div class="inputbox">
+                            <span>Email :</span>
+                            <input type="email" placeholder="example@example.com">
+                        </div>
+                        <div class="inputbox">
+                            <span>Address</span>
+                            <input type="text" placeholder="Floor - Street - Locality">
+                        </div>
+                        <div class="inputbox">
+                            <span>City</span>
+                            <input type="text" placeholder="Delhi">
+                        </div>
+                        <div class="flex">
+                            <div class="inputbox">
+                                <span>Country</span>
+                                    <input type="text" placeholder="India">
+                            </div>
+                            <div class="inputbox">
+                                <span>Zip Code</span>
+                                    <input type="text" placeholder="123 456">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <h3 class="title">Total Amount</h3>
+                        <div class="inputbox">
+                            <input type="text">
+                        </div>
+                </div>
+                <input type="submit" value="proceed to payment" class="submit-btn">
+            </form>
+        </div>
+    </body>
+</html>
