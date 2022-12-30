43478a423221f0fabae393c5ff05a698071cb07e
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2022-12-31 02:05:03 +0530

    Added .ico

--- faq.php
+++ faq.php
@@ -10,6 +10,7 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+  <link rel="icon" href="images/logo_greenLeaf.ico">
 </head>
 <body>
 
--- menu.php
+++ menu.php
@@ -1,31 +1,30 @@
-
 <script>
-      const uid = function(){
-        return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  const uid = function() {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  }
+  window.onload = () => {
+    // console.log(, "UID IS COMING")
+    if (!localStorage.getItem('userId')) {
+      const id = uid();
+      localStorage.setItem('userId', id);
+
+      function setCookie(name, value, exp_days) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exp_days * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+      }
+      setCookie('userId', id, 500);
+      window.location.reload();
     }
-    window.onload = () => {
-        // console.log(, "UID IS COMING")
-        if (!localStorage.getItem('userId'))
-        {
-            const id = uid();
-            localStorage.setItem('userId', id);
-            function setCookie(name,value,exp_days) {
-                var d = new Date();
-                d.setTime(d.getTime() + (exp_days*24*60*60*1000));
-                var expires = "expires=" + d.toGMTString();
-                document.cookie = name + "=" + value + ";" + expires + ";path=/";
-            }
-            setCookie('userId',id,500);
-            window.location.reload();
-        } 
-    }
+  }
 </script>
 
 <nav class="navbar navbar-expand-lg bg-light fixed-top">
   <div class="container-fluid">
     <a style="color: #367E18; font-size: 2.3rem" class="navbar-brand" href="index.php">
-      <img src="images\logo_greenLeaf.png" alt="Logo" width="50" height="">
-    GreenLeaf</a>
+      <img src="images\logo_greenLeaf.png" alt="Logo" width="45" height="">
+      GreenLeaf</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -33,58 +32,58 @@
       <ul class="navbar-nav ml-auto mb-2 mb-lg-0 p-2" style="font-size: 1.3rem; gap: 1rem">
         <li class="nav-item">
           <a data-toggle="tooltip" data-placement="bottom" title="Home" style="color: #38E54D" class="nav-link active" aria-current="page" href="index.php">
-          <img src="images\landing.png" alt="Home" width="25" height=""></a>
+            <img src="images/landing.png" alt="Home" width="30" height="" /></a>
         </li>
         <li class="nav-item">
           <a data-toggle="tooltip" data-placement="bottom" title="Shop" style="color: #38E54D" class="nav-link" href="store.php?category=all" target=_blank>
-          <img src="images\store.png" alt="Shop" width="25" height=""></a>
+            <img src="images/store.png" alt="Shop" width="28" height="" /></a>
         </li>
         <li class="nav-item">
           <a data-toggle="tooltip" data-placement="bottom" title="About Us" style="color: #38E54D" class="nav-link" href="about.php" target=_blank>
-          <img src="images\about-us.png" alt="About" width="25" height=""></a>
+            <img src="images/about-us.png" alt="About Us" width="28" height="" /></a>
         </li>
         <li class="nav-item">
           <a data-toggle="tooltip" data-placement="bottom" title="Contact Us" style="color: #38E54D" class="nav-link" href="contact.php" target=_blank>
-          <img src="images\envelope.png" alt="Contact" width="40" height="" style="margin-top:0px;"></a>
+            <img src="images/envelope.png" alt="Contact" width="44" height="" style="margin-top: -9px;" /></a>
         </li>
         <li class="nav-item">
-          <a data-toggle="tooltip" data-placement="bottom" title="FAQ" style="color: #38E54D" class="nav-link" href="faq.php">
-          <img src="images\FAQs.png" alt="FAQ" width="35" height="25"></a>
+          <a data-toggle="tooltip" data-placement="bottom" title="FAQs" style="color: #38E54D" class="nav-link" href="faq.php">
+            <img src="images/FAQs.png" alt="FAQs" width="35" height="25" /></a>
         </li>
+
         <li class="nav-item">
           <a data-toggle="tooltip" data-placement="bottom" title="Cart" style="color: #38E54D" class="nav-link" href="cart.php">
-            <img src="images\shopping-cart.png" alt="cart" width="35" height=""></a>
+            <img src="images/shopping-cart.png" alt="Cart" width="35" height="" /></a>
         </li>
       </ul>
     </div>
   </div>
+
   <div style="position:fixed; top:0; z-index:1; width:100%; background-color: #f1f1f1;">
-  <div style="width: 100%; height: 8px; background: #ccc;">
-    <div style="height: 8px; background: #54B435; width: 0%;" id="myBar"></div>
-  </div>  
-</div>
+    <div style="width: 100%; height: 8px; background: #ccc;">
+      <div style="height: 8px; background: #54B435; width: 0%;" id="myBar"></div>
+    </div>
+  </div>
 
-<script>
+  <script>
+    // When the user scrolls the page, execute myFunction 
+    window.onscroll = function() {
+      myFunction()
+    };
 
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+    function myFunction() {
+      var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+      var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      var scrolled = (winScroll / height) * 100;
+      document.getElementById("myBar").style.width = scrolled + "%";
+    }
+  </script>
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-</script>
-
-<script>  //tool tip calling
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();   
-});
-</script>
+  <script>
+    //tool tip calling
+    $(document).ready(function() {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+  </script>
 
 </nav>
-
-
-
-
