5aa3ce95fb77477a199692478e2c6b3846b59afe
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2022-12-31 20:37:25 +0530

    navbar icon (Bug Res)

--- menu.php
+++ menu.php
@@ -32,23 +32,23 @@
       <ul class="navbar-nav ml-auto mb-2 mb-lg-0 p-2" style="font-size: 1.3rem; gap: 1rem">
         <li class="nav-item">
           <a data-toggle="tooltip" data-placement="bottom" title="Home" style="color: #38E54D" class="nav-link active" aria-current="page" href="index.php">
-            <img src="images/landing.png" alt="Home" width="38" height="" style="margin-top:3px;"/></a>
+            <img src="images/home.png" alt="Home" width="38" height="" style="margin-top:3px;"/></a>
         </li>
         <li class="nav-item">
           <a data-toggle="tooltip" data-placement="bottom" title="Shop" style="color: #38E54D" class="nav-link" href="store.php?category=all" target=_blank>
-            <img src="images/store.png" alt="Shop" width="37" height="" style="margin-top:3px;"/></a>
+            <img src="images/shop.png" alt="Shop" width="37" height="" style="margin-top:3px;"/></a>
         </li>
         <li class="nav-item">
           <a data-toggle="tooltip" data-placement="bottom" title="About Us" style="color: #38E54D" class="nav-link" href="about.php" target=_blank>
-            <img src="images/about-us.png" alt="About Us" width="36" height="" style="margin-top:3px;"/></a>
+            <img src="images/AboutUs.png" alt="About Us" width="36" height="" style="margin-top:3px;"/></a>
         </li>
         <li class="nav-item">
           <a data-toggle="tooltip" data-placement="bottom" title="Contact Us" style="color: #38E54D" class="nav-link" href="contact.php" target=_blank>
-            <img src="images/envelope.png" alt="Contact" width="37" height="" style="margin-top:2px;" /></a>
+            <img src="images/ContactUs.png" alt="Contact" width="37" height="" style="margin-top:2px;" /></a>
         </li>
         <li class="nav-item">
           <a data-toggle="tooltip" data-placement="bottom" title="FAQs" style="color: #38E54D" class="nav-link" href="faq.php">
-            <img src="images/FAQs.png" alt="FAQs" width="53" height="" style="margin-top:1.5px;"/></a>
+            <img src="images/faq.png" alt="FAQs" width="53" height="" style="margin-top:1.5px;"/></a>
         </li>
 
         <li class="nav-item">
