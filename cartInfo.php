56770df1451fad20706efb82d861201fa84527f4
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2023-01-03 22:15:49 +0530

    bugResolved (minor) - Arpan

--- cartInfo.php
+++ cartInfo.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Redirecting....</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    
-<?php
+
+    <?php
     include 'DBconfig.php';
-    include 'menu_cart.php';
-     if($_REQUEST['cButton']=="check")      //if check button is pressed
+    //include 'menu_cart.php';
+    if ($_REQUEST['cButton'] == "check")      //if check button is pressed
     {
         $name = array();
         $email = array();
@@ -27,24 +29,20 @@
         $mobile = $_REQUEST['mobile'];
         $query = "SELECT * FROM customer WHERE mobile = '$mobile';";
         $exe = mysqli_query($con, $query);
-        if(mysqli_num_rows($exe) > 0)
-        {
-            while($row = mysqli_fetch_assoc($exe))
-            {
+        if (mysqli_num_rows($exe) > 0) {
+            while ($row = mysqli_fetch_assoc($exe)) {
                 $name = $row['name'];
                 $email = $row['email'];
                 $mobile = $row['mobile'];
                 $pincode = $row['pincode'];
                 $address = $row['address'];
             }
-        }
-        else{
+        } else {
             $name = "Name Not Found! Fill and Save!";
             $email = "Email Not Found! Fill and Save!";
             //$mobile = "";
             $address = " Address Not Found! Fill and Save";
             $pincode = "Pincode Not Found! Fill and Save";
-
         }
         // print ("I pressed ".$name);
         // print ("I pressed ".$email);
@@ -64,24 +62,24 @@
             </form>
             ";
 
-            //echo "You pressed check button.";
+        //echo "You pressed check button.";
     }   //end of if mobile found
-?>
+    ?>
 
-<script>          
-        document.querySelector("form").addEventListener("submit", function(evt){
-                        //evt.preventDefault();
-                        //document.getElementById("form1").submit();
-                    });
-                    
-                    // Just call the .click method of the button
-                    document.querySelector("input[type='submit']").click();
-</script>
+    <script>
+        document.querySelector("form").addEventListener("submit", function(evt) {
+            //evt.preventDefault();
+            //document.getElementById("form1").submit();
+        });
+
+        // Just call the .click method of the button
+        document.querySelector("input[type='submit']").click();
+    </script>
 
 
     <!-- if save button is pressed -->
     <?php
-    if($_REQUEST['cButton']=="save")   //if save button is pressed
+    if ($_REQUEST['cButton'] == "save")   //if save button is pressed
     {
         $name = $_REQUEST['name'];
         $email = $_REQUEST['email'];
@@ -89,20 +87,19 @@
         $pincode = $_REQUEST['pincode'];
         $address = $_REQUEST['address'];
         //echo $name;
-
         $query = "INSERT INTO customer (name, mobile, email, address, pincode) values ('$name','$mobile','$email','$address','$pincode');";
         $exe = mysqli_query($con, $query);
-        if(!$exe){
+        //header('location:cart.php');
+        if (!$exe) {
             die("Problem occured!");
-        }
-        else{
+        } else {
             header('location:cart.php');
         }
-        echo "You pressed save button.";
+        //echo "You pressed save button.";
     }
 
-?>
+    ?>
 
 </body>
-</html>
 
+</html>
