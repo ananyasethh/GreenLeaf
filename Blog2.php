bb5d3f15a18df6ea47e9ad36319a847807a570a2
Ananya Seth <sethananya58@gmail.com>
2023-01-06 07:09:52 +0530

    Blog 2-Ananya

---
+++ Blog2.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/logo_greenLeaf.ico">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="CSS/styles.css">
+    <title>Tree & Forest Activities To Do With Children</title>
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    <div class="container-fluid pt-5 mt-5">
+        <h1 style="color:#005555"class="display-5 text-center">Tree & Forest Activities To Do With Children</h1>
+        <div style = "margin: auto; width: 70%; padding: 10px;">
+            <img src="images/blog2.0.jpg" alt="Children" width="100%" height="">
+            <div style = "margin: auto; width: 68%; padding: 10px;">
+            <p style="font-size: 20px; line-height:1.5;">
+            Summer is here and for many of us that means more time spent outside with young ones. 
+            This can create the perfect scenario for tree and forest-themed activities.</p>
+            <p style="font-size: 20px; line-height:1.5;">As a bonus, time outdoors can boost kids' 
+            confidence, happiness, and focus, all by getting them up and moving and activating their senses. 
+            Studies have shown that spending time in nature can also spark a lifelong appreciation for the 
+            environment. And as any parent or caregiver knows, simply playing together outside can bring us closer together. </p>
+            <p style="font-size: 20px; line-height:1.5;">Outdoor activities don't have to be educational or super structured 
+            (the less structured the better!). The next time you and the little ones in your life visit a National Forest 
+            or local park, take a neighborhood walk, or even hang out in a backyard, try out the activities below. 
+            For all the adults without kids, unleash your inner child and have some quality outdoor playtime!</p>
+            </div>
+        </div>
+    </div>
+    <section>
+    <div>
+      <h2 style="padding-top:2%; padding-bottom:1.75%; color:#367E18; font-weight:bold;" class="text-center">Take a Sensory Stroll</h2>
+    <div style = "margin: auto; width: 70%;padding-left:13.5%;">
+        <img src="images/sensory-stroll.jpg" alt="Sensory Stroll" width="79.5%" height="">
+        <div style="padding-right:20%">
+        <p style="font-size: 20px; line-height:1.5;">This activity uses our senses and encourages kids to notice and connect with their 
+        surroundings. Before you begin, talk about what you might see, hear, smell, and touch (if there will be safe foraging or snacks, add in taste!)
+        on your sensory stroll.</p>
+        <p style="font-size: 20px; line-height:1.5;">Once you've reached the destination, start by drawing the child's attention to the ground. How does 
+        it feel under them? Reach down and touch it if you need to. Is it rough and hot, or soft and cool? Then focus on hearing, listening for different 
+        sounds, near and far. Continue with smell and sight, pausing every now and then to find new items to sense on your journey. Let the child set the pace.</p>
+        <p style="font-size: 20px; line-height:1.5;">For very young children, it may be best to focus on one sense, such as hearing. Encourage them to mimic what they hear.</p>
+        </div>
+    </div>
+    </div>
+
+    <div>
+      <h2 style="padding-top:2%; padding-bottom:1.75%; color:#367E18; font-weight:bold;" class="text-center">Observation Journal</h2>
+    <div style = "margin: auto; width: 70%;padding-left:13.5%;">
+        <img src="images/observatory.jpg" alt="Observation Journal" width="79.5%" height="">
+        <div style="padding-right:20%">
+        <p style="font-size: 20px; line-height:1.5;">Bring sketchpads and colored pencils or crayons on your outing (a magnifying glass may also be
+         helpful) and ask kids to draw or write about the natural items they observe. You can start right away or on a break once they've spotted 
+         something interesting. If possible, break down the item further by inspecting it up close, such as helping to find and draw the different 
+         parts of a flower.</p>
+        <p style="font-size: 20px; line-height:1.5;">Sensory activities like this one and the stroll above can help increase awareness and focus. 
+        To engage in this activity more at home, learn about the items you and the children observed online or in a book and talk about them 
+        together. For instance, if a child drew a plant, learn its name, range, and what eats it.</p>
+        </div>
+    </div>
+    </div>
+
+    <div>
+      <h2 style="padding-top:2%; padding-bottom:1.75%; color:#367E18; font-weight:bold;" class="text-center">Forest Bingo Scavenger Hunt</h2>
+    <div style = "margin: auto; width: 70%;padding-left:13.5%;">
+        <img src="images/forest.jpg" alt="Forest Bingo Scavenger Hunt" width="79.5%" height="">
+        <div style="padding-right:20%">
+        <p style="font-size: 20px; line-height:1.5;">Create a list with your child of all the items you can imagine seeing on your visit. Then 
+        create your own bingo boards on paper or cardboard, drawing in the items or writing by name. For example, your bingo board could include 
+        items such as a chipmunk, woodpecker, butterfly, clover, pinecone, muddy puddle, dog, and more. Remember to make all the boards different 
+        so no two are alike. Mark the items off with a pen or sticker as you come across them on your visit, getting “Bingo” on marked rows or 
+        entire boards.</p>
+        <p style="font-size: 20px; line-height:1.5;">If you have a very young child with a wild imagination, try incorporating an unlikely or 
+        mythical item on the board, making sure to only incorporate one per board. (Or go ahead and hide a sparkled unicorn figurine behind a bush!)</p>
+        </div>
+    </div>
+    </div>
+
+    <div>
+      <h2 style="padding-top:2%; padding-bottom:1.75%; color:#367E18; font-weight:bold;" class="text-center">Leaf Art</h2>
+    <div style = "margin: auto; width: 70%;padding-left:13.5%;">
+        <img src="images/leaf-art.jpg" alt="Leaf Art" width="79.5%" height="">
+        <div style="padding-right:20%">
+        <p style="font-size: 20px; line-height:1.5;"><em>Leaf prints: Watercolors can work as well. Try other natural items, such as pinecones 
+            and dandelions, for more unique painting patterns.</em></p>
+        <p style="font-size: 20px; line-height:1.5;">These activities will happen at home after your visit. While on your outing, 
+        collect several types of leaves for leaf-art projects. There are likely more than a thousand crafts to do with leaves, below are a few 
+        ideas to get started. All you will need are leaves, paper, paint, tape, crayons, and glue to do all three projects.</p>
+        <p style="font-size: 20px; line-height:1.5;">🟢<strong>Painted Leaf Prints:</strong> With paint, paint a leaf with a brush or dip 
+        it in a paint covered plate. Then push the leaf paint-side down onto craft paper or another intended surface. Keep going with more leaves 
+        and colors.</p>
+        <p style="font-size: 20px; line-height:1.5;">🟢<strong>Crayon Leaf Rubbing:</strong> On a table, tape a few leaves to a blank sheet of 
+        white paper. Then turn the paper over so the leaf side is against the table. Then shade the hidden leaf with the side of a paper-free crayon.</p>
+        <p style="font-size: 20px; line-height:1.5;">🟢<strong>Leaf Collage:</strong> Glue leaves or other found parts of trees (e.g., twigs, seed 
+        pods) to make unique collages. Design animals, houses, or whatever appeals to your little one in the moment.</p>
+        </div>
+    </div>
+    </div>
+
+    <div>
+      <h2 style="padding-top:2%; padding-bottom:1.75%; color:#367E18; font-weight:bold;" class="text-center">Climb Tree</h2>
+    <div style = "margin: auto; width: 70%;padding-left:13.5%;">
+        <img src="images/climb-tree.jpg" alt="Climb Tree" width="79.5%" height="">
+        <div style="padding-right:20%">
+        <p style="font-size: 20px; line-height:1.5;">Sometimes the simplest things are the best.</p>
+        </div>
+    </div>
+    </div>
+
+    <div>
+      <h2 style="padding-top:2%; padding-bottom:1.75%; color:#367E18; font-weight:bold;" class="text-center">Plant a Tree in Your Community</h2>
+    <div style = "margin: auto; width: 70%;padding-left:13.5%;">
+        <img src="images/plant-a-tree.jpg" alt="Plant a Tree in Your Community" width="79.5%" height="">
+        <div style="padding-right:20%">
+        <p style="font-size: 20px; line-height:1.5;">While it is rather difficult for individuals to plant trees on National Forests, 
+        there may be other types of planting events to join in your community.</p>
+        </div>
+    </div>
+    </div>
+   </section>
+
+   <p style="text-align:right; font-size:10px;">Resource from: nationalforest.org</p>
+   <footer>
+  <p style="color: #367E18" class="p-3 bg-light text-center">@GreenLeaf 2022</p>
+</footer>
+</body>
+</html>
