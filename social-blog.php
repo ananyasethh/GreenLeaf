e68804f92598fd5e88756b4c3b0cffa0a221f078
Ananya Seth <sethananya58@gmail.com>
2023-01-15 19:47:42 +0530

    Modifications - Ananya

--- social-blog.php
+++ social-blog.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Reviews</title>
+  <title>People and Plants</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="CSS/exhibit-styles.css">
